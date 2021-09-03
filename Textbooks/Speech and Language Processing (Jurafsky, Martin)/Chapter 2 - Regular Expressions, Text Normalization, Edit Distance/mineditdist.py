1ea6548f658282f62a7fa2e65bb76ce944de9d8a
Lucas Adelino <lcsadelino@gmail.com>
2021-09-03 17:30:17 -0400

    First commit

--- Textbooks/Speech and Language Processing (Jurafsky, Martin)/Chapter 2 - Regular Expressions, Text Normalization, Edit Distance/mineditdist.py
+++ Textbooks/Speech and Language Processing (Jurafsky, Martin)/Chapter 2 - Regular Expressions, Text Normalization, Edit Distance/mineditdist.py
@@ -1,6 +1,8 @@
+#TODO: Augment the minimum edit distance algorithm to output an alignment; you will need to store pointers and add a stage to compute the backtrace
+
 DEL_COST = 1
 INS_COST = 1
-SUB_COST_DIF = 1
+SUB_COST_DIF = 2
 SUB_COST_EQL = 0
 
 def min_edit_dist(source, target):
@@ -15,15 +17,13 @@ def min_edit_dist(source, target):
         matrix[0][j] = matrix[0][j-1] + INS_COST
 
     # Recurrence relation:
-    for i in range(1, n):
-        for j in range(1, m):
+    for i in range(0, n):
+        for j in range(0, m):
             sub_cost = 0
-            if source[i] == target[j]:
+            if source[i] == target[j]: #Check if we're subbing different or identical
                 sub_cost = SUB_COST_EQL
             else:
                 sub_cost = SUB_COST_DIF
-            matrix[i][j] = min(matrix[i-1][j] + DEL_COST, matrix[i-1][j-1] + sub_cost ,matrix[i][j-1] + INS_COST)
+            matrix[i+1][j+1] = min(matrix[i][j+1] + DEL_COST, matrix[i][j] + sub_cost ,matrix[i+1][j] + INS_COST)
 
     return matrix[n][m]
-
-print(min_edit_dist('leda', 'deal'))
